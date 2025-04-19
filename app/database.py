8efdd69ad77b56b16fccffc2257c670d586eb36a
bb220 <bb220@users.noreply.github.com>
2025-04-19 18:26:24 -0400

    Enable password reset email
    * Add reset tokens and new request workflow
    
    * add email support with sendgrid
    
    * update readme

--- app/database.py
+++ app/database.py
@@ -8,6 +8,9 @@ load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
 if not DATABASE_URL:
     raise ValueError("DATABASE_URL environment variable not set. Please check your .env file.")
 
@@ -23,3 +26,10 @@ else:
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
