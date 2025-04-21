39dd811d2820c25eb922dea387e223a5ded2c68f
bb220 <bb220@users.noreply.github.com>
2025-04-21 17:32:59 -0400

    Add updated and created fields (#3)

--- app/models.py
+++ app/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, DateTime, func
 from .database import Base
 
 class User(Base):
@@ -7,3 +7,5 @@ class User(Base):
     id = Column(Integer, primary_key=True, index=True)
     email = Column(String, unique=True, index=True)
     hashed_password = Column(String)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    updated_at = Column (DateTime(timezone=True), onupdate=func.now())
