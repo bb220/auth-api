19af90a6fef2f35a09e60f2fe0ecce834b9766c2
bb220 <bb220@users.noreply.github.com>
2025-04-27 14:59:13 -0400

    add api key test

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,3 +1,9 @@
+def test_access_without_api_key(client):
+    response = client.get("/register")  # No headers
+
+    assert response.status_code == 403 
+    assert "Forbidden. Invalid or missing API Key." in response.json()["detail"]
+
 def test_register_user(client, auth_headers, random_email):
     response = client.post("/register", json={
         "email": random_email,
