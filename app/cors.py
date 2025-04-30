68d31734cd736bd7d662ac9acdb39ab2fd226fb5
bb220 <bb220@users.noreply.github.com>
2025-04-30 18:02:23 -0400

    update CORS

--- app/cors.py
+++ app/cors.py
@@ -3,7 +3,7 @@ from fastapi.middleware.cors import CORSMiddleware
 # Define your trusted origins
 origins = [
     "http://localhost:3000",          # Local frontend
-    "https://v0-launcher-webapp-git-backend-integration-bb220s-projects.vercel.app/",       # Staging frontend 
+    "https://v0-launcher-webapp.vercel.app" # Deployed frontend
 ]
 
 def add_cors_middleware(app):
